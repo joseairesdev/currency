3407161c2bff40945d9aebdd38601eaaa9320a80
José Aires <joseairesdev@gmail.com>
2023-11-22 22:26:57 -0300

    init

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import styles from './home.module.css';
+
+const Home = () => {
+  return (
+    <main className={styles.container}>
+       <form>
+        <input type="text" />
+        <button type='submit'></button>
+       </form>
+    </main>
+  )
+}
+
+export default Home;
